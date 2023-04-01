bb3f399450b2e6d6894dfc66fc5f740403677665
_v_jason_ <vignonjason6@gmail.com>
2023-04-01 18:20:31 +0100

    update migration table of access

---
+++ database/migrations/2023_04_01_171029_create_accesses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accesses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+ 
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('id_role');
+ 
+            $table->foreign('id_role')->references('id')->on('roles');
+            $table->string('key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accesses');
+    }
+}
