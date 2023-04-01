63c7b63ef29dc7fdc530f6dc8bedc30bf8d7a492
Steve FASSEU <s.fasseu@gmail.com>
2023-04-01 16:34:00 +0100

    Make model of Role

--- database/migrations/2023_04_01_152701_create_roles_table.php
+++ database/migrations/2023_04_01_152701_create_roles_table.php
@@ -15,8 +15,6 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
-            $table->string('description');
             $table->timestamps();
         });
     }
