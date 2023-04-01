355cefdd214f3cfa60ccaf622ef4f1a87a4b4c5e
_v_jason_ <vignonjason6@gmail.com>
2023-04-01 17:24:44 +0100

    update migration of table access

--- database/migrations/2023_04_01_172003_create_roles_table.php
+++ database/migrations/2023_04_01_172003_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+class CreateAccessesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,15 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('accesses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
+            $table->unsignedBigInteger('id_user');
+ 
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('id_role');
+ 
+            $table->foreign('id_role')->references('id')->on('roles');
+            $table->string('key');
             $table->timestamps();
         });
     }
@@ -28,6 +33,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('accesses');
     }
 }
