66cf603f7405ae80737dddf9eae30198094274f7
Zaverie <zaveriefofou9@gmail.com>
2023-04-04 21:28:01 +0200

    update model Sale

--- app/Models/Sales.php
+++ app/Models/Sales.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Sales extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'Sales';
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+       'date',
+       'id_customer',
+       'id_car',
+       'raison'
+    ];
 }
