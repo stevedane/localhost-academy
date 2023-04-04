7d102b26516ce8fd27d9e6c4405213a5e1a9c1db
anais <cahathel@gmail.com>
2023-04-04 20:27:01 +0100

    model car updated

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cars';
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'imma',
+        'model',
+        'cylindre',
+        'marque',
+    ];
 }
