1874b6bb39fb1069219e3b27b75bf1d3acc9d39b
Nick Takafo <108556500+12leandry@users.noreply.github.com>
2023-04-04 20:58:53 +0100

    update seeder of models Customer and Rent

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class CustomerSeeder extends Seeder
 {
@@ -13,6 +16,10 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('customers')->insert([
+            'name' => 'nick',
+            'address' => 'bonaberi',
+            'email' => 'nick@gmail.com',
+        ]);
     }
 }
--- database/seeders/RentSeeder.php
+++ database/seeders/RentSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class RentSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class RentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('rents')->insert([
+            'start' => date('Y-m-d'),
+            'end' => date('Y-m-d'),
+            'id_car' => 1,
+            'id_customer' => 1,
+        ]);
     }
 }
