c46656730208d894690307bc6f792e606d9787b6
Nick Takafo <108556500+12leandry@users.noreply.github.com>
2023-04-04 20:27:37 +0100

    update models Customer and Rent

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'customers';
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'name',
+        'address',
+        'email',
+    ];
 }
--- app/Models/Rent.php
+++ app/Models/Rent.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Rent extends Model
 {
     use HasFactory;
+
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rents';
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'start',
+        'end',
+        'id_car',
+        'id_customer',
+    ];
 }
