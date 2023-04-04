eda748cb190ca637af9072af3df67340ec9e9a99
_v_jason_ <vignonjason6@gmail.com>
2023-04-04 20:28:20 +0100

    Update models of Access and Payment

--- app/Models/Acces.php
+++ app/Models/Acces.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Acces extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'acces';
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'id_user',
+        'id_role',
+        'key'
+    ];
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'payments';
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'date',
+        'amount',
+        'method',
+        'id_customer',
+        'id_car',
+        'id_service',
+        'service_type'
+    ];
 }
